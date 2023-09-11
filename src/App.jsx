8e33651501d82c10bec7d32d353ff55b57ddb941
yuumi <gin728emma218@gmail.com>
2023-09-11 13:16:39 +0900

    戻すボタンで、タスクを進行中に戻す

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,25 @@ export const App = () => {
         setTask('');
     }
 
+    //タスクを進行中にする関数
     const onClickProcessing = (index) => {
         const newProcessingTask = [...processingTask, untouchedTask[index]];
         untouchedTask.splice(index, 1);
         setProcessingTask(newProcessingTask);
     }
 
+    //タスクを完了する関数
     const onClickDone = (index) => {
         const newDoneTask = [...doneTask, processingTask[index]];
         processingTask.splice(index, 1);
         setDoneTask(newDoneTask);
+    }
+
+    //タスクを進行中に戻す関数
+    const onClickBack = (index) => {
+        const newProcessingTask = [...processingTask, doneTask[index]]
+        doneTask.splice(index, 1);
+        setProcessingTask(newProcessingTask);
     }
 
     return (
@@ -82,11 +91,11 @@ export const App = () => {
                 <div className="status-area__done">
                     <p className="title">完了したタスク</p>
                     <ul className="task-list">
-                        {doneTask.map((task) => {
+                        {doneTask.map((task, index) => {
                             return(
                                 <li className="task-list__item">
                                     <p>{task}</p>
-                                    <button>戻す</button>
+                                    <button onClick={() => onClickBack(index)}>戻す</button>
                                     <button>削除</button>
                                 </li>
                             );
