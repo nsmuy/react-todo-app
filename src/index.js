1da5510fb56a5cef54711a4059f3e467c1c3892c
yuumi <gin728emma218@gmail.com>
2023-09-12 00:02:47 +0900

    React18に対応

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDom from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
